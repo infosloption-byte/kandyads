e40d285b7c61d8799747de806a5bad5e9d5efbb5
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-08-31 03:48:15 +0530

    refactor: centralize contact details in src/data

---
+++ src/data/contact-details.js
@@ -0,0 +1,11 @@
+export const CONTACT_DETAILS = {
+  phones: ['0777 483 502', '0814 253 566'],
+  phoneHrefs: ['tel:+94777483502', 'tel:+94814253566'],
+  whatsapp: '94777483502',
+  whatsappHref: 'https://wa.me/94777483502',
+  emails: ['kandyads342@gmail.com', 'info@kandyads.lk'],
+  addresses: [
+    '155/E, Wathurakumbura Road, Kiribathkumbura, 20450',
+    'Warehouse - 150/B, Kahatagoda Road, Pilimathalawa.'
+  ]
+};
